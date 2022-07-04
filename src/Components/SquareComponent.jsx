2e025bb92ee98529906f18810ecb8ce361eb33f5
manishtmtmt <manisht.mt.mt@gmail.com>
2022-07-04 06:38:08 +0530

    react tic tac toe

---
+++ src/Components/SquareComponent.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SquareComponent = (props) => {
+  const classes = props.className ? `${props.className} square` : `square`;
+  return (
+    <span
+      className={classes + (props.state === "X" ? ` fc-aqua` : ` fc-white`)}
+      onClick={() => props.onClick(props.index)}
+    >
+      {props.state}
+    </span>
+  );
+};
+
+export default SquareComponent;
